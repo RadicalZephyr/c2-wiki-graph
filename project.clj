a4a8b3724d93c5b44cc468aea6046b7a70c5e04a
Geoff Shannon <geoffpshannon@gmail.com>
2015-10-30 09:48:59 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject c2-graph "0.1.0-SNAPSHOT"
+  :description "Mastery Cohort work with Bobby Norton on graphizing the c2 wiki"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [enlive "1.1.6"]
+                 [edgewise "0.1.0-SNAPSHOT"]])
---
+++ src/c2_graph/core.clj
@@ -0,0 +1,6 @@
+(ns c2-graph.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
