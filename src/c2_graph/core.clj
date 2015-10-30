949b168110bec185d72ffd2397e9c2699deff209
Geoff Shannon <geoffpshannon@gmail.com>
2015-10-30 10:50:24 -0500

    Add some functions for processing c2 files

--- src/c2_graph/core.clj
+++ src/c2_graph/core.clj
@@ -1,6 +1,20 @@
-(ns c2-graph.core)
+(ns c2-graph.core
+  (:require [clojure.java.io :as io]
+            [net.cgrand.enlive-html :as html]
+            [edgewise.core :as edgewise]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-c2-pages-seq []
+  (-> "pages"
+      io/resource
+      io/as-file
+      file-seq))
+
+(defn relative-to [root-file-path]
+  (fn [page-path]
+    (.relativize root-file-path page-path)))
+
+(defn transduce-files [root-file-path]
+  (->
+   (filter (complement (memfn isDirectory)))
+   (map (memfn toPath))
+   (map (relative-to root-file-path))))
